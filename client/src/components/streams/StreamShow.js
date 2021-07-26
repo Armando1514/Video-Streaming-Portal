a5821b875f8d5e2ef3719434777daaa8af52d90d
Armando1514 <armando@ferrara.link>
2021-07-26 12:02:58 +0200

    rtmp server for streaming added. flv.js library to read the stream from the rtmp server added to the StreamShow component.

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,16 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { connect } from "react-redux";
+import flv from "flv.js";
 import { fetchStream } from "../../actions";
 
 const StreamShow = ({ fetchStream, match, stream }) => {
+  let videoRef = useRef(null);
+  let player;
+
   useEffect(() => {
-    fetchStream(match.params.id);
+    const { id } = match.params;
+    fetchStream(id);
+    buildPlayer();
+
+    return player && player.destroy();
   }, [fetchStream, match]);
+
+  useEffect(() => {
+    buildPlayer();
+  }, [stream]);
+
+  const buildPlayer = () => {
+    const { id } = match.params;
+    if (player || !stream) return;
+    player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+
+    player.attachMediaElement(videoRef.current);
+    player.load();
+  };
 
   return !stream ? (
     <div>Loading</div>
   ) : (
     <div>
+      <video ref={videoRef} style={{ width: "100%" }} controls={true} />
       <h1>{stream.title}</h1>
       <h5>{stream.description}</h5>
     </div>
